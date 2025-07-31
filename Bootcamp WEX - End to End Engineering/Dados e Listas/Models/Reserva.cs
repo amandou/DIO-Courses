1232d9f2cee958e1cc8c41fc63debcfb1b62d4c7
Amanda <amanda.lrib@gmail.com>
2025-07-31 17:02:21 -0300

    feature: Add hotel accommodation challenge

---
+++ Bootcamp WEX - End to End Engineering/Dados e Listas/Models/Reserva.cs
@@ -0,0 +1,46 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                throw new Exception("Quantidade de hospedes não pode ser maior que a quantidade de pessoas na suite.");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            if (DiasReservados >= 10) valor *= Convert.ToDecimal(0.9);
+
+            return valor;
+        }
+    }
+}
